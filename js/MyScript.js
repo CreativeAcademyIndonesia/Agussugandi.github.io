442f0921d18e23801ce4698fd5e8912edb9c0662
CreativeAcademyIndonesia <83469189+CreativeAcademyIndonesia@users.noreply.github.com>
2021-05-01 01:17:19 +0700

    Update MyScript.js

--- js/MyScript.js
+++ js/MyScript.js
@@ -4,7 +4,7 @@ let scrollposition = window.scrollY;
 window.addEventListener('scroll', function () {
     scrollposition = window.scrollY
 
-    if (scrollposition >= 50) {
+    if (scrollposition >= 100) {
         nav.classList.replace('bg-light', 'bg-dark');
         nav.classList.replace('navbar-light', 'navbar-dark');
     } else {
